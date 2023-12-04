19b21e69d8a4fc53c8f502b01a4f501b71618898
Mohamed Nasir Mohamed Shaja <shaja.n@intervest.lk>
2023-12-04 23:59:11 +0530

    initial commit

---
+++ src/main/java/com/javainuse/droolbasic/DroolBasicApplication.java
@@ -0,0 +1,13 @@
+package com.javainuse.droolbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolBasicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javainuse/droolbasic/DroolBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainuse.droolbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
